6fd1f903884e876619d0f226935952782c2f5e70
mikagitti <mikagitti@gmail.com>
2024-10-17 14:33:14 +0300

    - Added styling where buttons should be. - Added button component which takes props. - Added hardcoded info for tile count, button size and boat loacation. - Added first version of coordination check.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,68 @@
+'use client';
+import SeaButton from "./components/sea-button";
+
+const tileCount = 5;
+const squereSize = 40;
+
+const pageStyle = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '100vw',
+  height: '100vh'
+}
+
+const playgroundStyle = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '500px',
+  height: '500px',
+  border: '1px solid black'
+}
+
+const buttonContainerStyle = {
+  display: 'grid',
+  gridTemplateColumns: `repeat(${tileCount}, 1fr)`,
+  gap: '10px'
+}
+
+const boatsLocation = ['A1', 'A2', 'A3'];
 
 export default function Home() {
+
+  const didItHit = (coord: string): boolean => {
+    if (boatsLocation.includes(coord)) {
+      return true;
+    }
+    return false;
+  }
+
+
   return (
-    <div>
-      <div style={{ width: '100%', height: '50px', backgroundColor: 'darkcyan' }}>
-        Let's Start!
+    <div style={pageStyle}>
+      <div style={playgroundStyle}>
+        <div style={buttonContainerStyle}>
+          {[...Array(tileCount * tileCount)].map((_, index) => {
+
+            return (
+              <SeaButton key={index} squereSize={squereSize} name={createCoordinationName()} didItHit={didItHit} />
+            );
+
+            function getRowAndColumn() {
+              const row: string = String.fromCharCode(65 + Math.floor(index / tileCount));
+              const col: number = (index % tileCount) + 1;
+              return { row, col };
+            }
+
+            function createCoordinationName() {
+              const { row, col }: { row: string; col: number; } = getRowAndColumn();
+              return `${row}${col}`;
+            }
+
+          })}
+        </div>
       </div>
     </div>
-
   );
 }
